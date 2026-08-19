eef971f1f11e44f2f2edacf4622c2e2845497035
Eduardo Lucas <dudlucas123@gmail.com>
2026-08-18 22:23:07 -0300

    Retornando estudos de SQL

--- Study MySQL/study.sql
+++ Study MySQL/study.sql
@@ -1,37 +1,38 @@
-CREATE DATABASE aula;
+USE estudos_sql; 
 
-USE aula;
+CREATE TABLE clientes (
+    id INT,
+    nome VARCHAR(100),
+    cpf VARCHAR(11), 
+    contato VARCHAR(20)
+    );
 
-CREATE TABLE cidade(
+INSERT INTO clientes VALUES(1, 'EDUARDO', '12345678910', '31912345678');
+INSERT INTO clientes VALUES(2, 'ALICE', '09876543210', '31909876543');
+INSERT INTO clientes VALUES(3, 'ANA', '10293847561', '31923459876');
+
+SELECT * FROM clientes WHERE nome = 'EDUARDO';
+SELECT id, cpf 
+FROM clientes 
+WHERE nome = 'EDUARDO';
+SELECT * 
+FROM clientes 
+LIMIT 2
+
+CREATE TABLE cidade (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100),
-    sigla char(3) UNIQUE
-);
+    sigla_estado VARCHAR(2) 
+    );
+INSERT INTO cidade (nome, sigla_estado) VALUES ('Belo horizonte', 'MG');
+INSERT INTO cidade (nome, sigla_estado) VALUES ('Belo horizonte', 'MG');
+UPDATE cidade SET nome = 'Sao Paulo', sigla_estado = 'SP' WHERE id = 2;
 
-INSERT INTO cidade (id, nome, sigla) VALUES (1, 'Minas gerais', 'MG');
+ALTER TABLE clientes
+MODIFY id INT PRIMARY KEY AUTO_INCREMENT;
 
-SELECT * FROM cidade;
+INSERT INTO clientes(nome, cpf, contato) VALUES ('Ariane', '12784391827', '31902945672');
+UPDATE clientes SET nome = UPPER(nome) WHERE id = 4;
 
-CREATE TABLE cliente(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    idcidade int,
-    constraint fkclientecidade FOREIGN KEY (idcidade) REFERENCES cidade(id)
-)
-
-INSERT INTO cliente VALUES (1, 'Eduardo', 1);
-
-SELECT * FROM cliente;
-
-ALTER TABLE cidade
-ADD  dddd CHAR(05);
-
-ALTER TABLE cidade
-change dddd ddd CHAR(5);
-
-ALTER TABLE cidade
-MODIFY ddd CHAR(2);
-
-UPDATE cidade SET ddd = 31 WHERE id = 1;
-
-SELECT * FROM cidade
+SELECT * FROM clientes WHERE nome in ('EDUARDO', 'ALICE');
+SELECT * FROM clientes WHERE nome LIKE 'EDUA%' or nome LIKE '%ICE';
